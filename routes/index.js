428c628b26b7b34ec6c1c4f821eff7c515ba5903
Nathan Anecone <nanecone.gmail.com>
2017-03-07 16:51:56 -0500

    changed layout

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,28 @@ const path = require('path');
 
 router.get('/', function(req, res){
   let tweets = tweetbank.list();
-  res.render('index', { tweets: tweets});
+  res.render('index', { tweets: tweets, });
+
 });
+
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetbank.find( {name: name} );
+  res.render( 'index', { tweets: list } );
+});
+
+
+
 
 // router.get('/stylesheets/style.css', function(req, res){
 //   res.sendFile("style.css", {root: path.join(__dirname, '../public', 'stylesheets')});
 // });
 
 router.use(express.static('public'));
+
+
+
 
 
 
