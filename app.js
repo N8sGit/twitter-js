14010e43a9a2d3eaf37e6a288154f15cf69f1de2
Jason <jason.miguel@gmail.com>
2017-03-07 12:24:29 -0500

    finished nunjucks render

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 const app = express();
 
-app.use('/news', function(req, res, next){
-  console.log(req.method, req.path)
+var locals = {
+  title: 'An Example',
+  people: [ { name: 'Gandalf'},
+            { name: 'Frodo'},
+            { name: 'Hermione'}
+          ]
+}
+nunjucks.configure('views', {
+  autoescape: true,
+  noCache: true
+});
+nunjucks.render('index.html', locals , function(err, output) {
+  console.log(output);
+});
 
+const people = [{name: 'Full'}, {name : 'Stacker'}, {name: 'Son'}];
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html' );
+
+
+
+app.use(function(req, res, next){
+  console.log(req.method, req.path)
 
   next()
 })
 
-
 app.get('/', function(req,res){
-  res.send('Welcome!!!')
+  res.render('index', {title: 'Hall of Fame', people: locals.people})
 })
 
 
-app.get('/news', function (req, res) {
+app.get('/index.html', function (req, res) {
   res.send('Get some news!')
 })
 
